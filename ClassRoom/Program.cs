a971cd0a178f40a821500df6c0aacf08f5cb83fe
Benjamin Kakar <benj3918@edu.easj.dk>
2018-09-05 14:47:20 +0200

    opgave 3

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +11,17 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            //Opret en instans af KlasseRum
+            KlasseRum kr = new KlasseRum();
+
+            kr.KlasseNavn = "3b";
+            kr.SemesterStart = new DateTime(2018,9,3);
+            kr.KlasseListe.Add(new Studerende("Benjamin",6,18));
+            kr.KlasseListe.Add(new Studerende("Arlind", 4, 24));
+            kr.KlasseListe.Add(new Studerende("Zaki", 6, 6));
+
         }
+
+        
     }
 }
