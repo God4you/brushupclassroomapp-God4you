532c750d59520ab4204e6c342422540aef075751
Benjamin Kakar <benj3918@edu.easj.dk>
2018-09-05 14:30:20 +0200

    opgave 2

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    class KlasseRum
+    {
+        public String KlasseNavn { get; set; }
+        public List<Studerende> KlasseListe { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+        public KlasseRum()
+        {
+            
+        }
+    }
+}
